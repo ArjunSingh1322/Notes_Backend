e2fca37791c792d7afdbb5c7028f4a29736e2a4f
Arjun Singh <as3388776@gmail.com>
2025-10-28 11:48:17 +0530

    update

--- app.js
+++ app.js
@@ -1,54 +1,79 @@
-// import express from "express"
+    // // import express from "express"
 
 
-// const app = express()
+    // // const app = express()
 
 
-// const PORT = process.env.PORT || 5047
+    // // const PORT = process.env.PORT || 5047
 
-// import "./config/db.js"
+    // // import "./config/db.js"
 
-// import user from "./model/user.model.js"
-// import notes from "./model/notes.model.js"
-
-
-
-
-// app.listen(PORT,()=>{
-//     console.log(`your backend is live on ${PORT}` )
-// })
+    // // import user from "./model/user.model.js"
+    // // import notes from "./model/notes.model.js"
 
 
 
 
+    // // app.listen(PORT,()=>{
+    // //     console.log(`your backend is live on ${PORT}` )
+    // // })
 
 
 
-import express from 'express'
-import cors from "cors"
-const app = express() 
-const PORT = process.env.PORT || 5047 
 
-// call database: 
-import "./config/db.js"
 
-import User from "./model/user.model.js"
-import Notes from "./model/notes.model.js"
 
-// routes: 
-import notesRoutes from "./routes/notes.routes.js"
 
-// middlewares: 
-app.use(cors())  
-app.use(express.json())
+    // import express from 'express'
+    // import cors from "cors"
+    // const app = express() 
+    // const PORT = process.env.PORT || 5047 
 
-app.use("/notes", notesRoutes)
+    // // call database: 
+    // import "./config/db.js"
 
-// ✅ Root route (for Vercel)
+    // import User from "./model/user.model.js"
+    // import Notes from "./model/notes.model.js"
+
+    // // routes: 
+    // import notesRoutes from "./routes/notes.routes.js"
+
+    // // middlewares: 
+    // app.use(cors())  
+    // app.use(express.json())
+
+    // app.use("/notes", notesRoutes)
+
+    // // ✅ Root route (for Vercel)
+    // app.get("/", (req, res) => {
+    // res.send("Backend is running successfully on Vercel!");
+    // });
+
+    // app.listen(PORT, ()=> {
+    //     console.log(`Server is running on port ${PORT}`)
+    // })
+
+
+
+
+
+    import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "./config/db.js";
+import notesRoutes from "./routes/notes.routes.js";
+
+dotenv.config();
+
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Backend is running successfully on Vercel!");
 });
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.use("/notes", notesRoutes);
+
+export default app;
